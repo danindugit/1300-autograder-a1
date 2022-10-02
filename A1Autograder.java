d1b94f5fa23f00f9c4439fc3fd97bd7dfb742063
dmarasin <dmarasin@uoguelph.ca>
2022-10-01 23:57:03 -0400

    inputted file name

--- A1Autograder.java
+++ A1Autograder.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class A1Autograder {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String fileName = "";
+
+        System.out.println("Enter student's filename:");
+        fileName = input.nextLine();
+
+
+    }
 }

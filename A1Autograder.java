7165178fca56d5349de33a870d57d562c73d7b0e
dmarasin <dmarasin@uoguelph.ca>
2022-10-03 23:05:09 -0400

    implemented gcc output

--- A1Autograder.java
+++ A1Autograder.java
@@ -1,8 +1,13 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class A1Autograder {
     private Scanner input;
     private String answer;
+    private String fileName;
 
     public A1Autograder (){
         this.input = new Scanner(System.in);
@@ -10,6 +15,28 @@ public class A1Autograder {
 
     public void runGrader(){
         System.out.println("Please open the student's file and ensure that it is named in the following fashion:\nLastNameFirstNameA1.c");
+        System.out.println("Please enter the file name:");
+        this.fileName = this.input.nextLine();
+        System.out.println("\nOutput of gcc compilation:\n");
+        CompileCprog(fileName);
+    }
+
+    public static void CompileCprog(String filename){
+        File dir = new File(".");
+        try {
+            String exeName = filename.substring(0, filename.length() - 2);
+            Process p = Runtime.getRuntime().exec("gcc -std=c99 -Wall " + filename, null, dir);
+//            p = Runtime.getRuntime().exec("./a.out", null, dir);
+            // Process p = Runtime.getRuntime().exec("ls", null, dir);
+            BufferedReader in = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                // System.out.println("makes it to the compiler");
+                System.out.println("\u001B[31m" + line + "\u001B[0m");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
